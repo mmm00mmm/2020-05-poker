464360d9ee6699b4cbf7bc455b12adbfb7447b17
mmm00mmm <54709088+mmm00mmm@users.noreply.github.com>
2020-05-06 16:12:04 +0900

    テストパタン追加

--- src/test/java/domain/hand/PokerHandJudgeTest.groovy
+++ src/test/java/domain/hand/PokerHandJudgeTest.groovy
@@ -1,25 +1,38 @@
 package domain.hand
 
-import fixture.FixtureCardList
+import domain.card.CardList
 import spock.lang.Specification
 import spock.lang.Unroll
 
 @Unroll
 class PokerHandJudgeTest extends Specification {
-    def "judge() #testName"() {
+    def judge() {
         expect:
-        PokerHandJudge.judge(cards) == expected
+        PokerHandJudge.judge(
+                CardList.create(Arrays.asList(cards.split(" ")))
+        ) == expected
 
         where:
-        testName             | cards                                    || expected
-        "onePair"            | new FixtureCardList().onePair            || PokerHand.ONE_PAIR.name
-        "twoPair"            | new FixtureCardList().twoPair            || PokerHand.TWO_PAIR.name
-        "threeOfAKind"       | new FixtureCardList().threeOfAKind       || PokerHand.THREE_OF_A_KIND.name
-        "straight"           | new FixtureCardList().straight           || PokerHand.STRAIGHT.name
-        "flush"              | new FixtureCardList().flush              || PokerHand.FLUSH.name
-        "fullHouse"          | new FixtureCardList().fullHouse          || PokerHand.FULL_HOUSE.name
-        "fourOfAKind"        | new FixtureCardList().fourOfAKind        || PokerHand.FOUR_OF_A_KIND.name
-        "straightFlush"      | new FixtureCardList().straightFlush      || PokerHand.STRAIGHT_FLUSH.name
-        "royalStraightFlush" | new FixtureCardList().royalStraightFlush || PokerHand.ROYAL_STRAIGHT_FLUSH.name
+        cards                         || expected
+        "H-Q D-A S-2 D-3 D-K"         || PokerHand.HIGH_CARDS.name
+        "D-2 C-Q D-J S-A D-10"        || PokerHand.HIGH_CARDS.name
+        "S-3 S-4 H-3 H-6 D-5"         || PokerHand.ONE_PAIR.name
+        "S-3 S-4 H-3 H-4 D-5"         || PokerHand.TWO_PAIR.name
+        "S-3 S-2 H-3 H-4 D-3"         || PokerHand.THREE_OF_A_KIND.name
+        "S-4 S-2 H-3 H-5 D-6"         || PokerHand.STRAIGHT.name
+        "S-A S-2 S-3 S-5 S-Q"         || PokerHand.FLUSH.name
+        "C-8 C-Q C-Q C-6 C-7"         || PokerHand.FLUSH.name              //FLUSHでONE_PAIR（トランプとしてはありえない）
+        "C-7 C-Q C-Q C-6 C-7"         || PokerHand.FLUSH.name              //FLUSHでTWO_PAIR（トランプとしてはありえない）
+        "C-Q C-Q C-Q C-6 C-7"         || PokerHand.FLUSH.name              //FLUSHでTHREE_OF_A_KIND（トランプとしてはありえない）
+        "S-A D-A C-A S-5 H-5"         || PokerHand.FULL_HOUSE.name
+        "S-A S-A S-A S-5 S-5"         || PokerHand.FULL_HOUSE.name         //FLUSHでFULL_HOUSE（トランプとしてはありえない）
+        "H-2 S-2 D-2 C-2 S-3"         || PokerHand.FOUR_OF_A_KIND.name
+        "S-4 S-4 S-4 S-4 S-8"         || PokerHand.FOUR_OF_A_KIND.name     //FLUSHでFOUR_OF_A_KIND（トランプとしてはありえない）
+        "S-4 S-4 S-4 S-4 D-10 D-10"   || PokerHand.FOUR_OF_A_KIND.name     //入力が多いパタン
+        "D-2 H-2 S-K C-A D-3 S-3 C-3" || PokerHand.FULL_HOUSE.name         //入力が多いパタン
+        "D-2 H-2 S-A C-A D-3 S-3 C-3" || PokerHand.THREE_OF_A_KIND.name    //入力が多いパタン
+        "S-6 S-7 S-8 S-9 S-10 S-J"    || PokerHand.STRAIGHT.name           //入力が多いパタン（同じスートが６枚なのでフラッシュにはならない）
+        "S-6 S-7 S-8 S-9 S-10"        || PokerHand.STRAIGHT_FLUSH.name
+        "D-A D-K D-Q D-J D-10"        || PokerHand.ROYAL_STRAIGHT_FLUSH.name
     }
 }
